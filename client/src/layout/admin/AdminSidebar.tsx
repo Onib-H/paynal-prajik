42472675591817e35121155ef782c688609355d6
Harold Aldovino <haroldaldovino@gmail.com>
2025-04-20 09:15:16 +0800

    ha

--- client/src/layout/admin/AdminSidebar.tsx
+++ client/src/layout/admin/AdminSidebar.tsx
@@ -1,12 +1,7 @@
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-<<<<<<< HEAD
-import React, { FC, Suspense, useEffect, useState } from "react";
-import { NavLink, useNavigate, Link } from "react-router-dom";
-=======
 import { FC, Suspense, lazy, useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
->>>>>>> 6b14be4acd94fd54f06750ae065ffa4142d7d88b
+import { NavLink, useNavigate, Link } from "react-router-dom";
 import Modal from "../../components/Modal";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { menuItems } from "../../constants/AdminMenuSidebar";
@@ -16,45 +11,21 @@ import { logout } from "../../services/Auth";
 import AdminProfile from "./AdminProfile";
 import hotelLogo from "../../assets/hotel_logo.png";
 
-<<<<<<< HEAD
-const AdminDetailSkeleton = React.lazy(
-  () => import("../../motions/skeletons/AdminDetailSkeleton")
-);
-
-interface AdminData {
-  name: string;
-  role: string;
-  profile_pic: string;
-}
-=======
-const AdminDetailSkeleton = lazy(() => import("../../motions/skeletons/AdminDetailSkeleton"));
->>>>>>> 6b14be4acd94fd54f06750ae065ffa4142d7d88b
-
-const AdminSidebar: FC<{ role: string }> = ({ role }) => {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const { setIsAuthenticated, setRole } = useUserContext();
-<<<<<<< HEAD
-  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true);
-
-  const [admin, setAdmin] = useState<AdminData>({
-    name: "",
-    role: "",
-    profile_pic: "",
-  });
-=======
+const AdminSidebar: FC = () => {
   const navigate = useNavigate();
->>>>>>> 6b14be4acd94fd54f06750ae065ffa4142d7d88b
+  const { setIsAuthenticated, role, setRole } = useUserContext();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const modalCancel = () => setIsModalOpen(!isModalOpen);
+  const modalCancel = () => setIsModalOpen(false);
 
   const { data: adminData, isLoading: profileLoading } = useQuery({
-    queryKey: ['adminProfile'],
+    queryKey: ["adminProfile"],
     queryFn: async () => {
       const response = await fetchAdminProfile();
       return response.data.data;
     },
   });
-  
+
   const { mutate: logoutMutation, isPending: logoutLoading } = useMutation({
     mutationFn: logout,
     onSuccess: (response) => {
@@ -65,10 +36,10 @@ const AdminSidebar: FC<{ role: string }> = ({ role }) => {
       }
     },
     onError: (error) => {
-      console.error(`Failed to error: ${error}`);
-    }
-  })
-  
+      console.error(`Failed to logout: ${error}`);
+    },
+  });
+
   const handleLogout = () => logoutMutation();
 
   const filteredMenuItems = menuItems.filter((item) => {
@@ -86,57 +57,32 @@ const AdminSidebar: FC<{ role: string }> = ({ role }) => {
 
   return (
     <>
-      <aside
-        className={`fixed top-0 left-0 flex flex-col p-4 bg-white text-black h-full transition-all duration-300  ${
-          isSidebarOpen ? "min-w-[300px]" : "w-[100px] items-center"
-        }`}
-      >
-        {/* Header section with toggle icon */}
-        <div
-          className={`border-b border-purple-200 pb-4 mb-4 flex items-center w-full ${
-            isSidebarOpen ? "justify-between" : "justify-center"
-          }`}
-        >
-          {isSidebarOpen && (
-            <Link to="/" className="flex items-center space-y-1">
-              <img
-                loading="lazy"
-                src={hotelLogo}
-                alt="Hotel Logo"
-                className="h-16 w-auto object-contain relative right-7"
-              />
-            </Link>
-          )}
-          <i
-            className="fa fa-bars text-3xl cursor-pointer"
-            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          ></i>
+      <aside className="fixed top-0 left-0 flex flex-col p-4 bg-white text-black h-full transition-all duration-300 min-w-[300px]">
+        {/* Header section with hotel logo */}
+        <div className="border-b border-purple-200 pb-4 mb-4 flex items-center justify-between w-full">
+          <Link to="/" className="flex items-center">
+            <img
+              loading="lazy"
+              src={hotelLogo}
+              alt="Hotel Logo"
+              className="h-16 w-auto object-contain"
+            />
+          </Link>
         </div>
 
         {/* Admin profile */}
-        <div
-          className={`p-2 pb-4 mb-4 bg-purple-100 rounded-md shadow-sm shadow-purple-300 w-full ${
-            !isSidebarOpen ? "flex justify-center" : ""
-          }`}
-        >
-          <Suspense fallback={<AdminDetailSkeleton />}>
-<<<<<<< HEAD
-            {admin ? (
-              <AdminProfile
-                admin={admin}
-                hideName={!isSidebarOpen} // Pass prop to hide name
-              />
+        <div className="p-2 pb-4 mb-4 bg-purple-100 rounded-md shadow-sm shadow-purple-300 w-full">
+          <Suspense fallback={<div>Loading...</div>}>
+            {profileLoading ? (
+              <div>Loading...</div>
             ) : (
-              <AdminDetailSkeleton />
+              adminData && <AdminProfile admin={adminData} />
             )}
-=======
-            {profileLoading ? <AdminDetailSkeleton /> : adminData && <AdminProfile admin={adminData} />}
->>>>>>> 6b14be4acd94fd54f06750ae065ffa4142d7d88b
           </Suspense>
         </div>
 
         {/* Menu items */}
-        <div className={`flex-grow overflow-y-auto p-2 w-full`}>
+        <div className="flex-grow overflow-y-auto p-2 w-full">
           <ul className="space-y-4">
             {filteredMenuItems.map((item, index) => (
               <li key={index}>
@@ -144,22 +90,18 @@ const AdminSidebar: FC<{ role: string }> = ({ role }) => {
                   to={item.link}
                   end={item.link === "/admin"}
                   className={({ isActive }) =>
-                    `flex items-center justify-baseline rounded-md cursor-pointer ${
+                    `flex items-center space-x-2 rounded-md cursor-pointer ${
                       isActive
-                        ? "border-r-3 border-purple-600 bg-purple-100/80 text-purple-700 font-bold"
+                        ? "border-r-4 border-purple-600 bg-purple-100/80 text-purple-700 font-bold"
                         : "hover:bg-purple-100/80 transition-colors duration-300"
-                    } ${
-                      !isSidebarOpen ? "justify-center space-x-0" : "space-x-2"
-                    }`
+                    } py-2 px-3`
                   }
                 >
                   <FontAwesomeIcon
                     icon={item.icon}
-                    className="p-2 w-5 h-5 text-xl text-left"
+                    className="w-5 h-5 text-xl"
                   />
-                  {isSidebarOpen && (
-                    <span className="text-md">{item.label}</span>
-                  )}
+                  <span className="text-md">{item.label}</span>
                 </NavLink>
               </li>
             ))}
@@ -170,18 +112,15 @@ const AdminSidebar: FC<{ role: string }> = ({ role }) => {
         <div className="px-3 py-4 w-full">
           <button
             onClick={() => setIsModalOpen(true)}
-            className={`w-full flex items-center ${
-              !isSidebarOpen ? "justify-center" : "space-x-3"
-            } py-2 px-3 rounded-md transition-all duration-300 text-red-600 hover:bg-black/15 cursor-pointer`}
+            className="w-full flex items-center space-x-3 py-2 px-3 rounded-md transition-all duration-300 text-red-600 hover:bg-black/15 cursor-pointer"
           >
             <i className="fa fa-sign-out-alt font-light"></i>
-            {isSidebarOpen && (
-              <span className="font-bold uppercase">Log Out</span>
-            )}
+            <span className="font-bold uppercase">Log Out</span>
           </button>
         </div>
       </aside>
 
+      {/* Logout Modal */}
       <Modal
         isOpen={isModalOpen}
         icon="fas fa-sign-out-alt"
@@ -189,16 +128,10 @@ const AdminSidebar: FC<{ role: string }> = ({ role }) => {
         description="Are you sure you want to log out?"
         cancel={modalCancel}
         onConfirm={handleLogout}
-<<<<<<< HEAD
-        loading={loading}
-        className={`bg-red-600 text-white hover:bg-red-700 font-bold uppercase text-sm px-6 py-3 rounded-md shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-300 cursor-pointer ${
-          loading ? "opacity-50 cursor-not-allowed" : ""
-        }`}
-=======
         loading={logoutLoading}
-        className={`bg-red-600 text-white hover:bg-red-700 font-bold uppercase text-sm px-6 py-3 rounded-md shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-300 cursor-pointer ${logoutLoading ? "opacity-50 cursor-not-allowed" : ""
-          }`}
->>>>>>> 6b14be4acd94fd54f06750ae065ffa4142d7d88b
+        className={`bg-red-600 text-white hover:bg-red-700 font-bold uppercase text-sm px-6 py-3 rounded-md shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-300 cursor-pointer ${
+          logoutLoading ? "opacity-50 cursor-not-allowed" : ""
+        }`}
         confirmText={
           logoutLoading ? (
             <>
